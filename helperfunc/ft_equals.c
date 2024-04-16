fdda754c580f1f879e28cea40815d8a5ea1cc73a
Eito Takahama <etakaham@c1r4s6.42tokyo.jp>
2024-04-16 21:07:33 +0900

    2024 0416 end

--- helperfunc/ft_equals.c
+++ helperfunc/ft_equals.c
@@ -1,31 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   ft_equals.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 20:31:21 by etakaham          #+#    #+#             */
-/*   Updated: 2024/04/16 20:31:21 by etakaham         ###   ########.fr       */
+/*   Created: 2024/04/16 20:48:36 by etakaham          #+#    #+#             */
+/*   Updated: 2024/04/16 20:48:36 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../includes/minishell.h"
+# include "../includes/minishell.h"
 
-static void	non_endl_echo(char **tokens)
+bool	ft_equals(const char *s1, const char *s2)
 {
-	return ;
-}
+	size_t	i;
 
-void	echo(char **tokens)
-{
-	if (!ft_equals(tokens[0], "echo"))
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		exit(1);
+		if (s1[i] == '\n')
+			return (true);
+		else if (s1[i] != s2[i])
+			return (false);
+		i++;
 	}
-	else if (ft_equals(tokens[1], "-n"))
-	{
-		non_endl_echo(tokens);
-	}
-	return ;
+	if (s1[i] == '\n')
+		return (true);
+	else if (s1[i] != s2[i])
+		return (false);
+	return (true);
 }
