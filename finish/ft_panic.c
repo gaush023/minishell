44e317047112b3389da7e2b2c669477a4d40e51b
Eito Takahama <etakaham@c1r4s2.42tokyo.jp>
2024-06-05 18:54:06 +0900

    finish norm edit

--- finish/ft_panic.c
+++ finish/ft_panic.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_panic.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 18:53:01 by etakaham          #+#    #+#             */
+/*   Updated: 2024/06/05 18:53:01 by etakaham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-void ft_panic(t_mini *mini, char *str,  enum e_err err_type)
+void	ft_panic(t_mini *mini, char *str, enum e_err err_type)
 {
-    write(2, "Error: ", 7);
-    write(2, str, ft_strlen(str));
-    write(2, "\n", 1);
-    if(mini != NULL)
-        free_all(mini, err_type);
-    exit(1);
+	write(2, "Error: ", 7);
+	write(2, str, ft_strlen(str));
+	write(2, "\n", 1);
+	if (mini != NULL)
+		free_all(mini, err_type);
+	exit(1);
 }
