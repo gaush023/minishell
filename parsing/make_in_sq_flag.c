7851d487ae0a786b3fe81548d27d0ef2a611446b
Shuga Agemura <sagemura@c1r7s8.42tokyo.jp>
2024-08-31 19:31:25 +0900

    tab

--- parsing/make_in_sq_flag.c
+++ parsing/make_in_sq_flag.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   make_in_sq_flag.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shuga <shuga@student.42.fr>                +#+  +:+       +#+        */
+/*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 20:17:36 by etakaham          #+#    #+#             */
-/*   Updated: 2024/07/30 02:53:17 by shuga            ###   ########.fr       */
+/*   Updated: 2024/08/31 19:30:44 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int	increment_case_quotetaions(int i, char *line, int *str_flag)
 
 static int	*mark_flag(char *line, int *str_flag, unsigned int pos, int flag)
 {
-	if (line[pos] == ' ')
+	if (line[pos] == ' ' || line[pos] == '\t')
 		str_flag[pos] = -1;
 	else if (flag == 1)
 		str_flag[pos] = -2;
@@ -86,3 +86,4 @@ int	*make_in_sq_flag(char *line, t_mini *mini)
 	str_flag[0] = -3;
 	return (str_flag);
 }
+	
