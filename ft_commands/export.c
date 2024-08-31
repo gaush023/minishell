5c69b68c3418f2f50beef00ba7082a16d2d2b564
Shuga Agemura <sagemura@c1r7s8.42tokyo.jp>
2024-08-31 16:09:57 +0900

    okay

--- ft_commands/export.c
+++ ft_commands/export.c
@@ -6,7 +6,7 @@
 /*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 18:48:21 by etakaham          #+#    #+#             */
-/*   Updated: 2024/08/27 19:20:49 by sagemura         ###   ########.fr       */
+/*   Updated: 2024/08/31 16:09:37 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ static void	insert_new_env(t_mini *mini, char **tokens)
 	t_env	*new_env;
 
 	new_env = my_calloc(1, sizeof(t_env), mini->m_node);
-	new_env->value = my_calloc(ft_strlen(tokens[1]), 1, mini->m_node);
-	ft_memcpy(new_env->value, tokens[1], ft_strlen(tokens[1]));
+	new_env->value = my_calloc(ft_strlen(tokens[1]) + 2, 1, mini->m_node);
+	ft_memcpy(new_env->value, tokens[1], ft_strlen(tokens[1]) + 2);
 	while (mini->env->next->next != NULL
 		&& !ft_eqaul_env(mini->env->next->value, new_env->value))
 		mini->env = mini->env->next;
